eeafcc9e653156de39c2804d8d479f7450510246
Willow Carlson-Huber <willow.ch@protonmail.com>
2022-02-11 17:28:19 -0600

    adingvidog

--- posts.sh
+++ posts.sh
@@ -3,7 +3,7 @@ cat ../content/feed.template.gmi > ../content/feed.gmi
 for i in $(ls -t *.gmi)
 do
 	echo $i
-	cat $i | tail -n +1 > ../content/posts/$(basename $i)
+	cat $i | tail -n +2 > ../content/posts/$(basename $i)
 	NAME=$(basename $i)
 	TITLE=$(cat $i | head -n 1)
 	DATE=$(git blame $i | cut -d ' ' -f 4 | tac | head -n 1)
