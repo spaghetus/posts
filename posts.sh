9f18deb5320b28b84a7bf063c19e41bdb1b2371a
Willow Carlson-Huber <willow.ch@protonmail.com>
2022-02-11 17:25:31 -0600

    added actual titles

--- posts.sh
+++ posts.sh
@@ -4,8 +4,9 @@ cat ../content/feed.template.gmi > ../content/feed.gmi
 for i in $(ls -t *.gmi)
 do
 	echo $i
-	cp $i ../content/posts
+	echo $i | tail -n +1 > ../content/posts/$(basename $i)
 	NAME=$(basename $i)
+	TITLE=$(echo $i | head -n 1)
 	DATE=$(git blame $i | cut -d ' ' -f 4 | tac | head -n 1)
-	echo "=> /posts/$NAME $DATE ${NAME%.gmi}" >> ../content/feed.gmi
+	echo "=> /posts/$NAME $DATE $TITLE" >> ../content/feed.gmi
 done
