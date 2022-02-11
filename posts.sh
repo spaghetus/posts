3a1567d48ed35ab58438f3c94d9092f15f4e17d6
Willow Carlson-Huber <willow.ch@protonmail.com>
2022-02-11 17:15:44 -0600

    a

--- posts.sh
+++ posts.sh
@@ -1,13 +1,11 @@
 #!bash
-cd posts
-# git pull
-cd ..
-cat content/feed.template.gmi > content/feed.gmi
-for i in $(ls -t posts/*.gmi)
+git pull origin main
+cat ../content/feed.template.gmi > ../content/feed.gmi
+for i in $(ls -t *.gmi)
 do
 	echo $i
-	cp $i content/posts
+	cp $i ../content/posts
 	NAME=$(basename $i)
 	DATE=$(git blame test-post.gmi | cut -d ' ' -f 4 | tac | head -n 1)
-	echo "=> /posts/$NAME $DATE ${NAME%.gmi}" >> content/feed.gmi
+	echo "=> /posts/$NAME $DATE ${NAME%.gmi}" >> ../content/feed.gmi
 done
